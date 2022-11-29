bd7ef5da7d81be4ed60bc96503ce010a4f4dd138
berhanez <birhanez@gmail.com>
2022-11-29 19:55:59 +0300

    exe

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
+if number < 0:
+    last_num = -last_num
+print(f"Last last_num of {number:d} is {last_num:d} and is ", end="")
 if last_num > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_num))
+    print("greater than 5")
 elif last_num == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_num))
-elif last_num < 6 and last_num != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_num))    
+    print("0")
+else:
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,6 @@
   For multiples of five, print Buzz instead of the number.
   For multiples of three and five, print FizzBuzz instead of the number.
   """
-
-
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
@@ -16,3 +14,4 @@ def fizzbuzz():
             print("Buzz ", end="")
         else:
             print("{} ".format(number), end="")
+            

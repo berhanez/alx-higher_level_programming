1f218d539a61aaa4a5fddb868923e68cd2e8d0f0
berhanez <birhanez@gmail.com>
2022-12-20 13:18:00 +0300

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Square class defined."""
 class Square:
+    """Square is made."""
     def __init__(self, size):
+        """Square is initialized"""
         self.__size = size

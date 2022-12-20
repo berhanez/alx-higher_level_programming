8dd2fc63d3103cbc959790b612578372876d18ef
berhanez <birhanez@gmail.com>
2022-12-20 12:31:42 +0300

    Task 103 - mimics byte code that gets area and circumference of circle. ALL ERRORS fixed, pycodestyle

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import math
 
+
 class MagicClass:
 
     def __init__(self, radius=0):
@@ -8,3 +9,11 @@ class MagicClass:
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("THE RADIUS MUST BE A DIGIT")
         self.__radius = radius
+
+    """GET AREA = (pi)(r)(r)"""
+    def area(self):
+        return (math.pi * self.__radius ** 2)
+
+    """GET Circumference = 2(pi)(r)"""
+    def circumference(self):
+        return (2 * math.pi * self.__radius)

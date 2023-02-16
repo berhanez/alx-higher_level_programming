b5f1af95f06cae0e5e110f4656e337cbff00e3a0
berhanez <birhanez@gmail.com>
2023-02-16 13:43:40 +0300

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- script creates mysql user user_0d_1 with all privileges
+-- script creates mysql user 
+-- user_0d_1 with all privileges
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
